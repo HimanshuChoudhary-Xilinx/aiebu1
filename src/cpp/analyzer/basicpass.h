b00d883e9da40cc11b246214fecbae6e2f71b091
Sonal Santan <sonal.santan@amd.com>
2025-10-15 16:56:19 -0700

    Framework for performing ctrlcode transforms (#141)
    * Early code base to support AIE2p ctrlcode transforms
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Use const reference for efficiency and delete fresh nodes to avoid memeory leak
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Move the pass manager to its own file
    
    Add a main for transform operations
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * More skeleton code for aiebu ctrlcode transform
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Update the PR to show example for new opcode (loadpdi) addition to an exisiting ctrlcode
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Add a note about older C++ coding style used for opcode printing
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Correct life cycle management of basic_node use in transform operations
    
    Added new code to preprocess legacy ELFs by adding addresses and fixing the flags
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Update the addresses to match the offsets for sections
    
    Ensure segments map to a single section
    
    Use std::ostringstream to force ELFIO to generate ELF layout
    
    CMake git should track main-ge and not master
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Intermediate working code
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * std::move of ELFIO::elfio does not work -- the address_translator in the ELFIO::header of the LHS gets corrupted when the RHS goes out of scope. Hence retain a persistent copy of the RHS as a class member of passmanager.
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Fix bugs related to transformed node insertion and existing node removal
    
    Check that a transaction binary is legacy format before attempting transforms
    
    Remove dead code
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * More transform code cleanup
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Remove more dead code
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * More transform code cleanup
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Improve documentation of basic_node memory management
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Add back the Boost path in transform to resolve build error on Windows
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    * Address clang-tidy remarks
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>
    
    ---------
    
    Signed-off-by: Sonal Santan <sonal.santan@amd.com>

---
+++ src/cpp/analyzer/basicpass.h
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+#ifndef AIEBU_BASICPASS_H_
+#define AIEBU_BASICPASS_H_
+
+#include <string>
+#include <list>
+#include <elfio/elfio.hpp>
+#include <elfio/elfio_section.hpp>
+
+namespace aiebu {
+
+enum class pass_kind {
+  print,
+  drop_preempt,
+  add_loadpdi
+};
+
+
+enum class basic_node_state {
+  original, // it is the original ctrlcode node from input ELF and is in
+            // pristine condition
+  dropped,  // it is the original ctrlcode node from input ELF but has been
+            // removed from the ctrlcode list
+  added,     // it is a new node which has been created and added to the ctrlcode
+            // list
+  zombie
+};
+
+template <typename aie2p_type>
+struct basic_node {
+  // m_op may point to an existing node in the current ELF when
+  // basic_node does not own the memory. The client would set
+  // m_state to "original" as a hint to basic_node.
+  // If the client explicitly allocates a aie2p_type node on the
+  // heap then they would set m_state to "added" which is used as
+  // a hint for basic_node to own the memory of m_op.
+  const aie2p_type *m_op;
+  size_t m_size;
+  basic_node_state m_state;
+
+  basic_node(const aie2p_type *op, size_t size,
+             basic_node_state state = basic_node_state::original)
+      : m_op(op), m_size(size), m_state(state) {}
+
+  basic_node(basic_node &&other) noexcept
+    : m_op(other.m_op), m_size(other.size), m_state(other.state) {
+    other.m_op = nullptr;
+    other.m_size = 0;
+    other.m_state = basic_node_state::zombie;
+  }
+
+  basic_node(const basic_node &other) = delete;
+  basic_node& operator =(basic_node const&other) = delete;
+  basic_node &operator=(basic_node &&other) = delete;
+
+  ~basic_node() {
+    if (m_state == basic_node_state::added) {
+      delete m_op;
+      m_op = nullptr;
+    }
+  }
+};
+
+
+template <typename aie2p_type> class aie2p_basicpass {
+public:
+  aie2p_basicpass() = default;
+  virtual ~aie2p_basicpass() = default;
+  // Delete copy and move constructors and assignment operators
+  aie2p_basicpass(const aie2p_basicpass&) = delete;               // Copy constructor
+  aie2p_basicpass& operator=(const aie2p_basicpass&) = delete;    // Copy assignment operator
+  aie2p_basicpass(aie2p_basicpass&&) = delete;                    // Move constructor
+  aie2p_basicpass& operator=(aie2p_basicpass&&) = delete;         // Move assignment operator
+
+  virtual void serialize() const {};
+  virtual void transform() = 0;
+};
+
+
+}
+
+#endif
---
+++ src/cpp/utils/transform/transform.cpp
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+#include <boost/format.hpp>
+#include <cxxopts.hpp>
+#include <string>
+#include <iostream>
+
+#include "analyzer/passmanager.h"
+#include "common/file_utils.h"
+#include "common/utils.h"
+
+namespace aiebu {
+
+cxxopts::ParseResult main_helper(int argc, const char* const *argv,
+                                 const std::string & executable,
+                                 const std::string & description)
+{
+  std::vector<std::string> subcmd_options;
+  cxxopts::Options global_options(executable, description);
+
+  try {
+    global_options.add_options()(
+        "j,transform",
+        "Name of JSON file with requested ctrlcode transform patterns",
+        cxxopts::value<std::string>()->default_value("unspecified"))(
+        "o,output", "Name of the output ELF file",
+        cxxopts::value<std::string>()->default_value("unspecified.elf"))(
+        "filename", "Input file name", cxxopts::value<std::string>())(
+        "help,h", "show help message and exit",
+        cxxopts::value<bool>()->default_value("false"))
+      ("d,debug", "dump ctrlcode after each transform", cxxopts::value<bool>()->default_value("false"))
+      ("v,version", "show version and exit", cxxopts::value<bool>()->default_value("false"));
+    global_options.parse_positional({"filename"});
+
+    auto result = global_options.parse(argc, argv);
+
+    subcmd_options = result.unmatched();
+
+    if (result.count("version")) {
+      std::cout << version_string();
+      return {};
+    }
+
+    if (result.count("help")) {
+      std::cout << global_options.help({"", executable}) << std::endl;
+      return {};
+    }
+
+    if (!result.count("filename"))
+      throw cxxopts::exceptions::missing_argument("filename");
+
+    return result;
+  }
+  catch (const cxxopts::exceptions::exception& e) {
+    auto errMsg = boost::format("Error parsing options: %s\n") % e.what() ;
+    throw std::runtime_error(errMsg.str());
+  }
+}
+
+} //namespace aiebu::utilities
+
+int main(int argc, char* argv[])
+{
+  const std::string executable = "aiebu-transform";
+  // -- Program Description
+  const std::string description = "aiebu ctrlcode transforming utility (aiebu-transform) for aie binaries";
+
+  cxxopts::ParseResult result;
+
+  try {
+    result = aiebu::main_helper(argc, argv, executable, description);
+  } catch (const std::exception& e) {
+    std::cout << e.what();
+    return 1;
+  }
+
+  if (!result.arguments().size())
+    return 1;
+
+  const std::vector<char> buffer = aiebu::readfile(result["filename"].as<std::string>());
+  if (aiebu::identify_buffer_type(buffer) !=
+      aiebu::aiebu_assembler::buffer_type::elf_aie2)
+    return 1;
+
+  ELFIO::elfio ebin;
+  ebin.load(result["filename"].as<std::string>());
+
+  // Run the transforms
+  aiebu::passmanager passm(ebin, result["debug"].as<bool>());
+  passm.run_transforms();
+
+  // Now save the ELF with transformed ctrlcode
+  if(!ebin.save(result["output"].as<std::string>()))
+    return 1;
+
+  return 0;
+}
