289d2bccbc761e4696faed3ef2c896acf4a28951
Soham Donwalkar <35574795+donwalkarsoham@users.noreply.github.com>
2025-11-21 15:29:48 -0800

    Add min supported version for Ubuntu and Boost (#175)
    * Add min supported version for Ubuntu and Boost
    
    Signed-off-by: Soham Donwalkar <soham.donwalkar@amd.com>
    
    * Add boost version check
    
    Signed-off-by: Soham Donwalkar <soham.donwalkar@amd.com>
    
    * Remove commented code
    
    Signed-off-by: Soham Donwalkar <soham.donwalkar@amd.com>
    
    ---------
    
    Signed-off-by: Soham Donwalkar <soham.donwalkar@amd.com>

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -4,15 +4,19 @@ if(POLICY CMP0144)
   cmake_policy(SET CMP0144 NEW)
 endif()
 
-# We use header only libraries
+# We use header only libraries for most components
+# Boost.Regex is header-only from 1.76.0+ (when using C++11 or later)
+# Require minimum Boost 1.76.0 for header-only Boost.Regex support
 if(${CMAKE_VERSION} VERSION_LESS "3.30")
-  find_package(Boost REQUIRED)
+  find_package(Boost 1.76.0 REQUIRED COMPONENTS regex)
 else()
-  find_package(Boost CONFIG REQUIRED)
+  find_package(Boost 1.76.0 CONFIG REQUIRED COMPONENTS regex)
 endif()
 
-message("-- Boost version: ${Boost_VERSION}")
-message("-- Boost include dir:${Boost_INCLUDE_DIRS}")
+message(STATUS "Boost version: ${Boost_VERSION}")
+message(STATUS "Boost include dir: ${Boost_INCLUDE_DIRS}")
+message(STATUS "Boost libraries: ${Boost_LIBRARIES}")
+message(STATUS "Boost.Regex: using header-only mode (Boost >= 1.76.0)")
 
 # Some later versions of boost spews warnings form property_tree
 # but can be disabled with this setting
